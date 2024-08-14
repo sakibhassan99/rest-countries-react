9e5ba38711a27012a0d81fff875e352fcf77e571
Sakib Hassan <sakibhassan.webdev@gmail.com>
2024-08-14 23:30:24 +0600

    Fix Simple Error

--- hooks/useTheme.js
+++ hooks/useTheme.js
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-export const useTheme = () =>
-  ([isDarkMode, setIsDarkMode] = useContext(ThemeContext));
+export const useTheme = () => {
+  const [isDarkMode, setIsDarkMode] = useContext(ThemeContext);
+  return [isDarkMode, setIsDarkMode];
+};
